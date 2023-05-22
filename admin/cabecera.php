4974b1f9694e89d43438c67a47abc7be0e3b5295
JUANIMAN <juanimandarin9@gmail.com>
2023-05-22 14:15:45 -0600

    Se acutalizo el formato de la cabecera de Adiministrar usuarios

--- admin/cabecera.php
+++ admin/cabecera.php
@@ -1,27 +1,23 @@
 <!DOCTYPE html>
 <html>
+
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
-    <link rel="stylesheet" href="../css/style.css">
+	<link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+	<link rel="stylesheet" href="../css/style1.css">
 	<title></title>
 </head>
+
 <body>
-<header>
-	Administrador de Usuarios
-</header>
-
-<nav>
-	<ul>
-		<li class="dropdown">
-			<a href="javascript:void(0)" class="dropbtn">Usuario</a>
-			<div class="dropdown-content">
-				<a href="http://localhost/files/admin/add_user.php">Agregar</a>
-				<a href="http://localhost/files/admin/consultar.php">Consultar</a>
-			</div>
-		</li>
-	</ul>
-</nav>
-
-<section id="contenedor">
+	<header>
+		<h1>Administrador de Usuarios</h1>
+		<nav>
+			<ul>
+				<li><a href="add_user.php">Agregar</a></li>
+				<li><a href="consultar.php">Consultar</a></li>
+				<li><a class="logout" href="../connection/logout.php">Salir</a></li>
+			</ul>
+		</nav>
+	</header>
+	<section id="contenedor">
